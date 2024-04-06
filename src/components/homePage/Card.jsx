32a82c6f8cbe90505a0f08304a343d0dcbd3a3dd
shohan <shohan.ece17@gmail.com>
2024-04-07 03:53:02 +0600

    setting up the cards

--- src/components/homePage/Card.jsx
+++ src/components/homePage/Card.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
 export default function Card() {
+  const [isFavourite, setIsFavourite] = useState(true);
   return (
     <div className="card bg-base-100 shadow-xl col-span-1">
       <figure>
@@ -8,15 +11,26 @@ export default function Card() {
         />
       </figure>
       <div className="card-body p-4">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <h2 className="card-title">Shoes!</h2>
+        <div className="card-actions justify-start">
+          <div className="badge badge-accent badge-outline">Fashion</div>
+          <div className="badge badge-accent badge-outline">Products</div>
         </div>
+        <div className="w-fit border-2 border-pink-300 rounded-md bg-pink-200 px-2 py-1">
+          {isFavourite ? (
+            <button className="flex items-center gap-2">
+              Added to Favourites
+              <FaHeart fill="red" />
+            </button>
+          ) : (
+            <button className="flex items-center gap-2">
+              Add to Favourites
+              <FaHeart fill="violet" />
+            </button>
+          )}
+        </div>
+
+        <p>If a dog chews shoes whose shoes does he choose?</p>
       </div>
     </div>
   );
