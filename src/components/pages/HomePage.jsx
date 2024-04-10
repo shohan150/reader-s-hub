f5589b333d3ddf90d03ca1a597ce0171f0e9c4a5
shohan <shohan.ece17@gmail.com>
2024-04-11 00:15:18 +0600

    kono mote joratali diye chalalam.

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,10 +4,9 @@ import CardSkeleton from "../homePage/CardSkeleton";
 
 export default function HomePage() {
   const searchParams = { searchTerm: "popular-books" };
-  // const fav = {};
-  // const bookddd = useFavourites(fav);
+
   const { books, loading } = useBooksAPI(searchParams);
-  console.log(books);
+  // console.log(books);
   return (
     <div className="flex-1 grid lg:grid-cols-3 grid-cols-2 lg:gap-6 gap-4">
       {books.map((book, index) =>
--- src/components/search/SearchModal.jsx
+++ src/components/search/SearchModal.jsx
@@ -1,12 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import Card from "../../components/homePage/Card";
+import CardSkeleton from "../../components/homePage/CardSkeleton";
 
 export default function SearchModal() {
   const [isHovered, setIsHovered] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchedBooks, setSearchedBooks] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   const handleHover = () => {
     setIsHovered(!isHovered);
   };
+
+  const handleChange = (value) => {
+    setSearchTerm(value);
+  };
+
+  async function fetchSearchedBooks(searchTerm) {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching the booklist...",
+      });
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=3&key=${
+          import.meta.env.VITE_GOOGLE_BOOKS_API_KEY
+        }`
+      );
+      if (!response.ok) {
+        const errorMessage = `Fetching weather data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+      const data = await response.json();
+      // console.log(data);
+      // console.log(response);
+      setSearchedBooks(data.items);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: "",
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetchSearchedBooks(searchTerm);
+  }, [searchTerm]);
+
   return (
     <>
       <button
@@ -25,8 +72,19 @@ export default function SearchModal() {
             type="text"
             placeholder="Search Here"
             className="my-4 w-full border-2 rounded py-1 px-2"
+            onChange={(e) => {
+              handleChange(e.target.value);
+            }}
           ></input>
+          {searchedBooks.map((book, index) =>
+            loading.state ? (
+              <CardSkeleton key={index} />
+            ) : (
+              <Card key={book?.id} book={book} />
+            )
+          )}
         </div>
+
         <form method="dialog" className="modal-backdrop">
           <button></button>
         </form>
